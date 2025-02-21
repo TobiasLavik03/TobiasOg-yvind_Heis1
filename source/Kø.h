6363ff985dfdc24ab2cc981e9accce6c69a8a121
Tobias Lavik <tobialav@stud.ntnu.no>
2025-02-21 20:49:02 +0100

    siste endringer før branch

--- source/Kø.h
+++ source/Kø.h
@@ -17,6 +17,6 @@ typedef struct{
 int neste_stopp;
 
 void legg_til_etasje_i_kø(Kø *aKø, Etasje aEtasje);
-void fjern_etasjer_fra_kø(Kø &aKø, int aEtasje);
+void fjern_etasjer_fra_kø(Kø *aKø, int aEtasje);
 void oppdater_neste_stopp();
 
