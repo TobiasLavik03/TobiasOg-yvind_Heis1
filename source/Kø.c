5a16816cabf40c44b059df9eb0d6c31e4517fe20
Tobias Lavik <tobialav@stud.ntnu.no>
2025-02-21 21:04:58 +0100

    Siste endringer før checkout

--- source/Kø.c
+++ source/Kø.c
@@ -1,25 +1,11 @@
 #include "Kø.h"
 
-<<<<<<< HEAD
-void legg_til_etasje_i_kø(Kø *aKø, Etasje aEtasje) {
-  aKø->lengde++;
-  aKø->liste = (Etasje *)realloc(
-      aKø->liste,
-      aKø->lengde * sizeof(Etasje)); // finner plass i minnet med plass til ant
-                                     // bytes fra der aKø->liste peker + ant
-                                     // etasjer ganget med etasjestørrelsen
-  aKø->liste[aKø->lengde - 1] = aEtasje;
-}
-
-
 void oppdater_neste_stopp(Kø *aKø) {
     // logikk for å finne neste stopp, ser på avstand til etasje, og retning på ønsket
 }
-
 void tøm_kø(Kø *aKø) {
     // tømmer hele køen
 }
-=======
 void legg_til_etasje_i_kø(Kø *aKø, Etasje aEtasje){
     aKø->lengde++;
     aKø->liste = (Etasje*)realloc(aKø->liste, aKø->lengde * sizeof(Etasje)); //finner plass i minnet med plass til ant bytes fra der aKø->liste peker + ant etasjer ganget med etasjestørrelsen
@@ -29,4 +15,3 @@ void legg_til_etasje_i_kø(Kø *aKø, Etasje aEtasje){
 void fjern_etasjer_fra_kø(Kø *aKø, int aEtasje){
     
 };
->>>>>>> 9a31b138ad306493d009e7e1126933c0de87c48b
