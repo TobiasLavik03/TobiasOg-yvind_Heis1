8a6f0b769060838917bd2fac629dda6866268a7b
abc <abc>
2025-02-21 14:43:36 +0100

    fredag 21.02

--- source/Kø.h
+++ source/Kø.h
@@ -1,5 +1,11 @@
 typedef struct{
     int etasje;
-    int retning;
-} etasje;
+    int retning;        // -1 nedover, 1 oppover, 0 ingen retning (for heispanelet)
+} Etasje;
+
+int neste_stopp;
+
+
+void legg_til_etasje_i_kø();
+void oppdater_neste_stopp();
 
